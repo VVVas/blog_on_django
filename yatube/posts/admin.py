26ba29bb45e352d4218f1b45e357a477ed6dcc2d
VVVas <v@vvvas.ru>
2023-04-11 00:05:54 +0300

    Comments in admin

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Group, Post
+from .models import Comment, Group, Post
+
+
+class CommentInLine(admin.StackedInline):
+    model = Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -9,6 +13,9 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
+    inlines = [
+        CommentInLine,
+    ]
 
 
 class GroupAdmin(admin.ModelAdmin):
